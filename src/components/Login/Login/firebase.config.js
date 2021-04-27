aafa374b4111a871f13cf74359c41dd2c92b08a0
Masud Rana <masudranawd2019@gmail.com>
2021-04-27 11:56:11 +0600

    doctors-portal-client

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAp_msK1q63U_BGTyGUvr_mM2cNPQRwDWU",
+  authDomain: "doctors-portal-923d5.firebaseapp.com",
+  projectId: "doctors-portal-923d5",
+  storageBucket: "doctors-portal-923d5.appspot.com",
+  messagingSenderId: "567956882494",
+  appId: "1:567956882494:web:d49baffe2459880360fd6f"
+};
+  export default firebaseConfig;
